c29aa52bc892f2a9e7d5669c20fd467bae4b2518
Hubbard, Bobby <Robert.Hubbard@garmin.com>
2021-08-12 14:53:23 -0500

    publish

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 #IPADDR=3.22.239.246 #TEST
 IPADDR=3.141.65.150 #PROD
 
-scp -ri /Users/hubbardr/.ssh/LightsailDefaultKey-us-east-2.pem ~/dev/django_projects/northlandobgyn/manage.py ~/dev/django_projects/northlandobgyn/faqs ~/dev/django_projects/northlandobgyn/locations ~/dev/django_projects/northlandobgyn/header ~/dev/django_projects/northlandobgyn/footer ~/dev/django_projects/northlandobgyn/manage.py ~/dev/django_projects/northlandobgyn/northlandobgyn ~/dev/django_projects/northlandobgyn/providers ~/dev/django_projects/northlandobgyn/resources ~/dev/django_projects/northlandobgyn/services bitnami@${IPADDR}:/opt/bitnami/projects/northlandobgyn/
+#scp -ri /Users/hubbardr/.ssh/LightsailDefaultKey-us-east-2.pem ~/dev/django_projects/northlandobgyn/manage.py ~/dev/django_projects/northlandobgyn/faqs ~/dev/django_projects/northlandobgyn/locations ~/dev/django_projects/northlandobgyn/header ~/dev/django_projects/northlandobgyn/footer ~/dev/django_projects/northlandobgyn/manage.py ~/dev/django_projects/northlandobgyn/northlandobgyn ~/dev/django_projects/northlandobgyn/providers ~/dev/django_projects/northlandobgyn/resources ~/dev/django_projects/northlandobgyn/services bitnami@${IPADDR}:/opt/bitnami/projects/northlandobgyn/
 
 #settings only
 #scp -ri /Users/hubbardr/.ssh/LightsailDefaultKey-us-east-2.pem ~/dev/django_projects/northlandobgyn/northlandobgyn/settings.py bitnami@${IPADDR}:/opt/bitnami/projects/northlandobgyn/northlandobgyn/settings.py
@@ -10,7 +10,7 @@ scp -ri /Users/hubbardr/.ssh/LightsailDefaultKey-us-east-2.pem ~/dev/django_proj
 #html only
 #scp -ri /Users/hubbardr/.ssh/LightsailDefaultKey-us-east-2.pem ~/dev/django_projects/northlandobgyn/northlandobgyn/templates/ bitnami@${IPADDR}:/opt/bitnami/projects/northlandobgyn/northlandobgyn/templates/
 
-#scp -ri /Users/hubbardr/.ssh/LightsailDefaultKey-us-east-2.pem ~/dev/django_projects/northlandobgyn_static/ bitnami@${IPADDR}:/opt/bitnami/projects/
+scp -ri /Users/hubbardr/.ssh/LightsailDefaultKey-us-east-2.pem ~/dev/django_projects/northlandobgyn_static/ bitnami@${IPADDR}:/opt/bitnami/projects/
 
 #scp -ri /Users/hubbardr/.ssh/LightsailDefaultKey-us-east-2.pem ~/dev/django_projects/northlandobgyn/startup.sh bitnami@${IPADDR}:~
 
